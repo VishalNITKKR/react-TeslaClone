2848d0989269997e0ff1d4ab3a05ccf16a21c439
VishalNITKKR <vishal_52010011@nitkkr.ac.in>
2021-12-10 01:49:56 +0530

    addition

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+// function component
+function Header() {
+    return (
+        <div>
+            Header
+        </div>
+    )
+}
+// function component
+export default Header
